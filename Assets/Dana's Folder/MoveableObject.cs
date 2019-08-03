561c7ea43fd268f749e3a8dd07ad5a18a4cb5aec
danalemay <danalemay@users.noreply.github.com>
2019-08-02 22:39:16 -0400

    player ccan pickup

---
+++ Assets/Dana's Folder/MoveableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour
+{
+    public Transform guide;
+    public GameObject tempParent;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mother fakin left mouse button bitch");
+        }*/
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        transform.position = guide.position;
+        transform.rotation = guide.rotation;
+        transform.parent = tempParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        transform.parent = null;
+        transform.position = guide.position;
+    }
+}
--- Assets/Dana's Folder/PickUpRotate.cs
+++ Assets/Dana's Folder/PickUpRotate.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PickUpRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject item;
+    public Transform guide;
+    
+    void Update()
     {
+       if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mother fakin left mouse button bitch");
+            OnMouseDown();
+        }
+       else if (Input.GetMouseButtonUp(0))
+        {
+            OnMouseUp();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+        Debug.Log("mouse down");
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i].tag == "Pickupable")
+            {
+                Debug.Log("hit pickup");
+                item = hitColliders[i].gameObject;
+                item.GetComponent<Rigidbody>().useGravity = false;
+                item.GetComponent<Rigidbody>().isKinematic = true;
+                item.transform.position = guide.position;
+                item.transform.rotation = guide.rotation;
+                item.transform.parent = guide.transform;
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position = guide.position;
     }
 }
