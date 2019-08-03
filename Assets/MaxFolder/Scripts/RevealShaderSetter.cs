f7c269fea3be07a37d44ca81e5e2374d85fa7623
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-08-03 00:30:06 -0400

    Monster World texture changing effect

---
+++ Assets/MaxFolder/Scripts/RevealShaderSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class RevealShaderSetter : MonoBehaviour
+{
+    public float Radius = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_Position", transform.position);
+        Shader.SetGlobalFloat("_Radius", Radius);
+    }
+}
