f0fc49704eb5b80b7efc0742f03399dff2b69e34
danalemay <danalemay@users.noreply.github.com>
2019-08-02 21:24:38 -0400

    spawn points

---
+++ Assets/Dana's Folder/Respawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform respawnpoint;
+    
+    public void RespawnPlayer()
+    {
+        //yeet player
+    }
+}
---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{    
+    void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10f);
+        
+        for(int i = 0; i< hitColliders.Length; i++)
+        {
+            if(hitColliders[i].tag == "Player")
+            {
+                hitColliders[i].GetComponent<Respawn>().respawnpoint = transform;
+            }
+        }
+    }
+}
