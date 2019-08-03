42642bd929c6f86820838e74924a3206b9a45551
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-08-02 21:20:50 -0400

    Hooking up inputs to player - looking & moving

---
+++ Assets/MaxFolder/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Pawn ControlledPawn;
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    protected virtual void HandleInput()
+    {
+        ControlledPawn?.PassMovementHorizontal(Input.GetAxis("Horizontal"));
+        ControlledPawn?.PassMovementVertical(Input.GetAxis("Vertical"));
+        ControlledPawn?.PassJump(Input.GetButtonDown("Jump"));
+        ControlledPawn?.PassSprint(Input.GetButton("Sprint"));
+        ControlledPawn?.PassCrouch(Input.GetButton("Crouch"));
+
+        ControlledPawn?.PassLook(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+    }
+}
