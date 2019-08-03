7a527c840fb7f4337feb02a50cb2e8585af41910
Melody Morse <14865089+MelodyMorse@users.noreply.github.com>
2019-08-03 13:58:45 -0400

    isPositionReaced, walkTowards in state machine

--- Assets/Melody's Folder/StateMachine.cs
+++ Assets/Melody's Folder/StateMachine.cs
@@ -5,43 +5,73 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     private IEnumerable state;
-    
+    private GameObject player;
+    public Vector3 lastKnownPosition = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         state = DebugLogState("Hello");
-        Debug.Log("Starting state machine");
-        StartCoroutine(RunStateMachine());
+        //Debug.Log("Starting state machine");
+        //StartCoroutine(RunStateMachine());
     }
 
     public IEnumerator RunStateMachine()
     {
         while(state != null)
         {
-            Debug.Log("State Machine Running");
+            //Debug.Log("State Machine Running");
             foreach (var c in state)
             {
-                Debug.Log("in foreach loop");
+               // Debug.Log("in foreach loop");
                 yield return c;
             }
         
         }
     }
+    #region States
+    private IEnumerable WalkTowardsLastKnownPosition()
+    {
+        bool positionReached = false;
+        bool playerSensed = false;
+        bool demonEyeOpened = false;
+        while (!positionReached && !playerSensed && !demonEyeOpened)
+        {
+            //until reached
 
+            //or sense player
+            yield return null;
+
+
+        }
+    }
     private IEnumerable DebugLogState(string message)
     {
         while (true)
         {
             yield return new WaitForSeconds(1);
-            Debug.Log(message);
+            //Debug.Log(message);
+
 
         }
     }
-    
-    
+    #endregion
+    #region Scenario Checks
+    private bool isPositionReached(Vector3 p)
+    {
+        if (Vector3.Distance(p, transform.position) < 0.01)
+        {
+            return true;
+        }
+        return false;
+    }
+    #endregion
+
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("position reached: " + isPositionReached(Vector3.zero));
     }
 }
