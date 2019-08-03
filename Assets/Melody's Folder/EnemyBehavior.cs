ded9b090adc4cd9d4d26b13d4b788608418f2ebf
Melody Morse <14865089+MelodyMorse@users.noreply.github.com>
2019-08-02 20:26:33 -0400

    testplayerprefabmade
    also more setup in melody's room

---
+++ Assets/Melody's Folder/EnemyBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    private Vector3? lastKnownPosition = null;
+     
+    //variables for finding player
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EyeIsOpenOpen())
+        {
+            //isplayerinattackingrange...
+        }
+        else
+        {
+            //isLastKnownExists
+        }
+    }
+    private bool EyeIsOpenOpen()
+    {
+        return false;
+    }
+
+
+}
+
+
+
