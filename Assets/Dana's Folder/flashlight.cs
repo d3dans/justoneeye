4bd2575b614bdb5bd44e183d877121328dacd2e0
danalemay <danalemay@users.noreply.github.com>
2019-08-04 00:08:39 -0400

    flashlight

---
+++ Assets/Dana's Folder/flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    public bool flashlighton = false;
+    public GameObject flashlightObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Flashlight"))
+        {
+            Debug.Log("flashlight");
+            if(flashlighton)
+            {
+                flashlightObject.SetActive(false);
+                flashlighton = false;
+            }
+            else
+            {
+                flashlightObject.SetActive(true);
+                flashlighton = true;
+            }
+        }
+    }
+}
